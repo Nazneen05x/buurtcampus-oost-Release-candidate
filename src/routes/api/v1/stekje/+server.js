e5ec3761b65e9837c4c6750c15f2f8276cef83a1
Krijn Hoetmer <krijn@qontent.nl>
2023-03-10 12:07:36 +0100

    Add stekje endpoint

---
+++ src/routes/api/v1/stekje/+server.js
@@ -0,0 +1,42 @@
+import { GraphQLClient, gql } from 'graphql-request'
+import { HYGRAPH_KEY, HYGRAPH_URL } from '$env/static/private'
+
+import { responseInit } from '$lib/server/responseInit'
+
+const hygraph = new GraphQLClient(HYGRAPH_URL, {
+  headers: {
+    Authorization: `Bearer ${HYGRAPH_KEY}`,
+  },
+})
+
+export async function GET({ url }) {
+  let slug = url.searchParams.get('slug') ?? false
+
+  const query = gql`
+    query getStekje($slug: String) {
+      stekje(slug: $slug) {
+        aanmelddatum
+        createdAt
+        giftig
+        id
+        landvanherkomst
+        naam
+        publishedAt
+        slug
+        updatedAt
+        verpotten
+        voeding
+      }
+      stekjesConnection {
+        pageInfo {
+          hasNextPage
+          hasPreviousPage
+          pageSize
+        }
+      }
+    }
+  `
+  console.log(query)
+  const data = await hygraph.request(query, { first, skip, orderBy })
+  return new Response(JSON.stringify(data), responseInit)
+}
